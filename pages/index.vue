784bc9ace4448eec9ae758f52ef1bb6662b78fff
acidjazz <acidjazz@gmail.com>
2019-07-15 23:26:06 -0500

    :clown_face: logo mockup

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
-<template>
-  <div style="text-align: center; padding-top: 50vh;"> under construction </div>
+<template lang="pug">
+.flex.items-center.justify-center.flex-col.h-screen
+  .svg.w-32
+    include ../static/logo.svg
+  .font-slab.text-2xl.uppercase.text-allports mindful family solutions
+  .font-merri.text-seagreen.italic.text-xl
+    span empower
+    i.mdi.mdi-circle-medium
+    span connect
+    i.mdi.mdi-circle-medium
+    span support
+
 </template>
 
 <script>
