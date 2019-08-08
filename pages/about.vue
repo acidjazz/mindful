8543069f4f83fd59fa31f0060120b41b69ea0b83
acidjazz <acidjazz@gmail.com>
2019-08-08 18:31:51 -0500

    📱 mobile support for about

--- pages/about.vue
+++ pages/about.vue
@@ -5,17 +5,18 @@
       .text-4xl.lg_text-6xl.text-white.text-center.pb-6(v-in-vp).vp-y.vp-d-2 About your coach
   .below.lg_m-16.m-4
     .container
-      .flex.-px-12
-        .w-1_2.p-12.flex.flex-col.justify-around.text-lg
-          p.mb-8(v-in-vp).vp-y A graduate with her Master of Social Work, Alexis now has over ten years of experience working with families. Alexis has explored, analyzed and created effective parenting techniques for families based on what she has experienced as s a nanny, child behavior specialist, social worker, and mental health therapist. Alexis believes that the core components of strong families lie in empowerment, personal connections and internal support systems. A foundational belief of her coaching philosophy is that these principles can all be derived from the concept of mindfulness, where one can learn to become more aware of the present moment by being in touch with and acknowledging thoughts, feelings [physical and emotional] as well as the surrounding environment.
-          p(v-in-vp).vp-y Alexis recently departed from being a youth and family mental health therapist to focus solely on coaching. The majority of her work has been processing trauma with kiddos and their families, teaching regulation of emotions, and increasing positive behaviors and parenting techniques in families.
-        .w-1_2.p-12.flex.items-center
+      .flex.flex-col.lg_flex-row.lg_-px-12.my-8
+        .lg_w-1_2.lg_p-12.flex.items-center
           img(src="/alexis.jpg")(v-in-vp).vp-y
+        .lg_w-1_2.lg_p-12.flex.flex-col.justify-around.text-lg
+          .my-8
+            p.mb-8(v-in-vp).vp-y A graduate with her Master of Social Work, Alexis now has over ten years of experience working with families. Alexis has explored, analyzed and created effective parenting techniques for families based on what she has experienced as s a nanny, child behavior specialist, social worker, and mental health therapist. Alexis believes that the core components of strong families lie in empowerment, personal connections and internal support systems. A foundational belief of her coaching philosophy is that these principles can all be derived from the concept of mindfulness, where one can learn to become more aware of the present moment by being in touch with and acknowledging thoughts, feelings [physical and emotional] as well as the surrounding environment.
+            p(v-in-vp).vp-y Alexis recently departed from being a youth and family mental health therapist to focus solely on coaching. The majority of her work has been processing trauma with kiddos and their families, teaching regulation of emotions, and increasing positive behaviors and parenting techniques in families.
 
-      .flex.-px-12
-        .w-1_2.p-12
+      .flex.flex-col.lg_flex-row.lg_-px-12.my-8
+        .lg_w-1_2.lg_p-12.flex.items-center
           img(src="/alexis2.jpg")(v-in-vp).vp-y
-        .w-1_2.p-12
+        .lg_w-1_2.lg_p-12
           .flex.m-4(v-in-vp).vp-y
             .bg-seagreen.w-1.mr-4
             .text-2xl.font-bold.mb-2.text-seagreen 10 fun facts about me
