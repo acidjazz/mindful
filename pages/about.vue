c09adfda1c21a5970ad7a1b02e39fecee51af390
acidjazz <acidjazz@gmail.com>
2019-08-08 18:19:07 -0500

    🚧 about page take 1

---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.page.page-about.pt-18
+  .hero.bg-jump.h-screen
+    .container.flex.justify-center.items-center.lg_items-end.h-screen
+      .text-4xl.lg_text-6xl.text-white.text-center.pb-6(v-in-vp).vp-y.vp-d-2 About your coach
+  .below.lg_m-16.m-4
+    .container
+      .flex.-px-12
+        .w-1_2.p-12.flex.flex-col.justify-around.text-lg
+          p.mb-8(v-in-vp).vp-y A graduate with her Master of Social Work, Alexis now has over ten years of experience working with families. Alexis has explored, analyzed and created effective parenting techniques for families based on what she has experienced as s a nanny, child behavior specialist, social worker, and mental health therapist. Alexis believes that the core components of strong families lie in empowerment, personal connections and internal support systems. A foundational belief of her coaching philosophy is that these principles can all be derived from the concept of mindfulness, where one can learn to become more aware of the present moment by being in touch with and acknowledging thoughts, feelings [physical and emotional] as well as the surrounding environment.
+          p(v-in-vp).vp-y Alexis recently departed from being a youth and family mental health therapist to focus solely on coaching. The majority of her work has been processing trauma with kiddos and their families, teaching regulation of emotions, and increasing positive behaviors and parenting techniques in families.
+        .w-1_2.p-12.flex.items-center
+          img(src="/alexis.jpg")(v-in-vp).vp-y
+
+      .flex.-px-12
+        .w-1_2.p-12
+          img(src="/alexis2.jpg")(v-in-vp).vp-y
+        .w-1_2.p-12
+          .flex.m-4(v-in-vp).vp-y
+            .bg-seagreen.w-1.mr-4
+            .text-2xl.font-bold.mb-2.text-seagreen 10 fun facts about me
+          ul.ml-4(v-in-vp).vp-y
+            li I did my Master’s Thesis on teaching social skills to kiddos on the Autism Spectrum.
+            li I specialize in helping families with kiddos who have emotional and developmental difficulties/differences.
+            li I think kiddos have so much to offer parents in the realm of teaching!
+            li My family coaching philosophy is rooted in a mindfulness practice coupled with positive and peaceful parenting techniques.
+            li I love to cook, bake and swim!
+            li I have 3 fur children currently with my husband-Bart, Gigi Bear and Ginger and am currently pregnant with my first human child! Due November of 2019.
+
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
